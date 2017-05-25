76dc816e7b789b93c9f68fbe93590810d40f3bf9
Matthew Cottrell <mattcottrell93@hotmail.co.uk>
2017-05-25 17:11:09 +0000

    Games are part created

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,30 @@
+class GamesController < ApplicationController
+   
+    def new
+       @game = Game.new
+    end
+    
+    def create
+       @game = Game.new(game_params)
+       @game.user_id = current_user.id
+       if @game.save
+           flash[:success] = "Game successfully saved"
+           redirect_to game_path(@game)
+       else
+           flash[:danger] = @game.errors.full_messages.join(",")
+           render 'new'
+       end
+    end
+    
+    def show
+        @game = Game.find(params[:id])
+    end
+   
+   
+   
+   private
+   def game_params
+      params.require(:game).permit(:user_id, :title) 
+   end
+    
+end
