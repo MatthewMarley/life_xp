13955ad167a9aab39d49fff1cef48cf5f57dc210
Matthew Cottrell <mattcottrell93@hotmail.co.uk>
2017-05-25 10:08:00 +0000

    Added User Authentication and Signup Form

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   
   root to: 'pages#home'
   get '/about', to: 'pages#about'
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   
 end
